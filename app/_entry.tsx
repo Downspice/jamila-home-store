4eb30ba68e6cfe7b8393d3925655c01672e8c101
jamilahomedev <jamilahomedev@gmail.com>
2025-09-15 15:22:29 +0100

    fix the entry txx file

--- app/_entry.tsx
+++ app/_entry.tsx
@@ -6,31 +6,31 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as SplashScreen from 'expo-splash-screen';
  
 
-// export default function AppEntry() {
-//   const [showOnboarding, setShowOnboarding] = useState<boolean | null>(null);
+export default function AppEntry() {
+  const [showOnboarding, setShowOnboarding] = useState<boolean | null>(null);
 
-//   useEffect(() => {
-//     const checkOnboarding = async () => {
-//       const seen = await AsyncStorage.getItem("hasSee22nboarding");
-//       console.log("storage>>>>",seen)
-//       setShowOnboarding(seen !== "true");
-//     };
-//     checkOnboarding();
-//   }, []);
+  useEffect(() => {
+    const checkOnboarding = async () => {
+      const seen = await AsyncStorage.getItem("hasSee22nboarding");
+      console.log("storage>>>>",seen)
+      setShowOnboarding(seen !== "true");
+    };
+    checkOnboarding();
+  }, []);
 
-//   if (showOnboarding === null) return null;
-//   console.log("showOnboarding:", showOnboarding);
+  if (showOnboarding === null) return null;
+  console.log("showOnboarding:", showOnboarding);
 
-//   if (showOnboarding) {
-//     return (
-//       <OnboardingScreen
-//         onFinish={async () => {
-//           await AsyncStorage.setItem("hasSeenOnboarding", "true");
-//           setShowOnboarding(false);
-//         }}
-//       />
-//     );
-//   }
+  if (showOnboarding) {
+    return (
+      <OnboardingScreen
+        onFinish={async () => {
+          await AsyncStorage.setItem("hasSeenOnboarding", "true");
+          setShowOnboarding(false);
+        }}
+      />
+    );
+  }
 
-//   return <Slot />;
-// }
+  return <Slot />;
+}
